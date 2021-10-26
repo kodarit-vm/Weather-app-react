47c83bb8936756f167ea0f0d4387cd8716e63f3d
Vekkumasa <ylirasku@gmail.com>
2021-10-26 19:03:46 +0300

    Niin valmis kuin voi olla

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { getForecast } from './request'
-import { formatTime } from './utils/formatTime'
+import { formatTime, formatDay } from './utils/formatTime'
 import WeatherCard from './WeatherCard'
 
 const useStyles = makeStyles(() => ({
@@ -17,6 +17,13 @@ const useStyles = makeStyles(() => ({
       display: 'flex',
       flexDirection: 'row',
       flexWrap: 'wrap'
+    },
+    weatherSelected: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      fontSize: '32px',
+      padding: 30
     }
 }))
 
@@ -37,9 +44,13 @@ const Weather = () => {
 
     const selectedForecast = forecasts.list.find(f => f.dt === focusedForecastDT)
 
+    const changeFocusedForecast = (forecast) => {
+      setFocusedForecastDT(forecast.dt)
+    }
+
     const renderWeatherItem = (forecast) => {
       return (
-        <div style={{ padding: 10 }} key={forecast.dt}>
+        <div style={{ padding: 10 }} key={forecast.dt} onClick={() => changeFocusedForecast(forecast)}>
           <WeatherCard forecast={forecast}/>
         </div>
       )
@@ -57,6 +68,13 @@ const Weather = () => {
           <div className={classes.center}>
             Aurinko laskee klo: {formatTime(forecasts.city.sunset)}
           </div>
+        </div>
+        <div className={classes.weatherSelected}>
+          <div> {formatDay(selectedForecast.dt)} </div>
+          <div> {formatTime(selectedForecast.dt)} </div>
+          <div> {Math.round(selectedForecast.main.temp)}° </div>
+          <div> {selectedForecast.weather[0].description} </div>
+          <img src={`http://openweathermap.org/img/wn/${selectedForecast.weather[0].icon}.png`} alt="Logo" />
         </div>
         <div className={classes.row}>
           {forecasts.list.map(forecast => renderWeatherItem(forecast))}
